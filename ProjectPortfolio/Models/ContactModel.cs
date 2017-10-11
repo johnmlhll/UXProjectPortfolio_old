fdd82eba0075fa52a7e385464fab4b7ad2134707
John Mulhall <johnmlhll@yahoo.com>
2017-10-11 17:54:00 +0100

    2nd Commit - Shared Layout Footer View

---
+++ ProjectPortfolio/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectPortfolio.Models
+{
+    /// <summary>
+    /// Class definition: ContactModel Class to DataModel the Data Inputs from Online Contact Form to XML DB
+    /// </summary>
+    public class ContactModel
+    {
+        //no args constructor
+        public ContactModel() {}
+
+        /*
+         * Model Properties
+         */
+    }
+}
